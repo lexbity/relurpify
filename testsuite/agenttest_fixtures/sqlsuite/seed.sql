072cb990962cd51f417cd46fa76ad023b71a05ef
prestin liu <liuprestin@gmail.com>
2025-12-17 13:25:45 -0800

    Add testsuite/agenttest_fixtures/sqlsuite/seed.sql

---
+++ testsuite/agenttest_fixtures/sqlsuite/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (id, name) VALUES (1, 'alice');
+INSERT INTO users (id, name) VALUES (2, 'bob');
+
+INSERT INTO posts (id, user_id, title) VALUES (1, 1, 'hello');
+INSERT INTO posts (id, user_id, title) VALUES (2, 1, 'world');
+INSERT INTO posts (id, user_id, title) VALUES (3, 2, 'first');
+
