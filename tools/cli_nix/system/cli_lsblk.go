0c62b95be9b5217cc3c8be8e565d841c31291297
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/system/cli_lsblk.go

--- tools/cli_nix/system/cli_lsblk.go
+++ tools/cli_nix/system/cli_lsblk.go
@@ -1,12 +1,12 @@
 package system
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewLsblkTool exposes the lsblk CLI.
-func NewLsblkTool(basePath string) framework.Tool {
+func NewLsblkTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_lsblk",
 		Description: "Lists block devices via lsblk.",
