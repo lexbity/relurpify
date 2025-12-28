4156ad845562fe2f5f97e5dfe30b0168533eed29
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update framework/search_test.go

--- framework/search_test.go
+++ framework/search_test.go
@@ -1,39 +1,49 @@
-package framework
+package framework_test
 
 import (
 	"context"
+	"github.com/lexcodex/relurpify/framework/core"
+	"github.com/lexcodex/relurpify/framework/search"
 	"strings"
 	"testing"
 )
 
 type stubCodeIndex struct {
-	chunks []*CodeChunk
+	chunks []*core.CodeChunk
 }
 
-func (s *stubCodeIndex) GetFileMetadata(string) (*FileMetadata, bool)           { return nil, false }
+func (s *stubCodeIndex) GetFileMetadata(string) (*core.FileMetadata, bool)      { return nil, false }
 func (s *stubCodeIndex) ListFiles() []string                                    { return nil }
-func (s *stubCodeIndex) GetSymbolsByName(string) ([]SymbolLocation, error)      { return nil, nil }
-func (s *stubCodeIndex) GetSymbolDefinition(string) (*SymbolLocation, error)    { return nil, nil }
-func (s *stubCodeIndex) GetSymbolReferences(string) ([]SymbolLocation, error)   { return nil, nil }
-func (s *stubCodeIndex) GetFileDependencies(string) []string                    { return nil }
-func (s *stubCodeIndex) GetDependents(string) []string                          { return nil }
-func (s *stubCodeIndex) GetChunksForFile(string) []*CodeChunk                   { return nil }
-func (s *stubCodeIndex) GetChunkByID(string) (*CodeChunk, bool)                 { return nil, false }
-func (s *stubCodeIndex) FindChunksByName(string) []*CodeChunk                   { return nil }
-func (s *stubCodeIndex) FindChunksByFileAndRange(string, int, int) []*CodeChunk { return nil }
-func (s *stubCodeIndex) SearchChunks(string, int) []*CodeChunk                  { return s.chunks }
-func (s *stubCodeIndex) BuildIndex(context.Context) error                       { return nil }
-func (s *stubCodeIndex) UpdateIncremental([]string) error                       { return nil }
-func (s *stubCodeIndex) Save() error                                            { return nil }
-func (s *stubCodeIndex) Version() string                                        { return "" }
+func (s *stubCodeIndex) GetSymbolsByName(string) ([]core.SymbolLocation, error) { return nil, nil }
+func (s *stubCodeIndex) GetSymbolDefinition(string) (*core.SymbolLocation, error) {
+	return nil, nil
+}
+func (s *stubCodeIndex) GetSymbolReferences(string) ([]core.SymbolLocation, error) {
+	return nil, nil
+}
+func (s *stubCodeIndex) GetFileDependencies(string) []string         { return nil }
+func (s *stubCodeIndex) GetDependents(string) []string               { return nil }
+func (s *stubCodeIndex) GetChunksForFile(string) []*core.CodeChunk   { return nil }
+func (s *stubCodeIndex) GetChunkByID(string) (*core.CodeChunk, bool) { return nil, false }
+func (s *stubCodeIndex) FindChunksByName(string) []*core.CodeChunk   { return nil }
+func (s *stubCodeIndex) FindChunksByFileAndRange(string, int, int) []*core.CodeChunk {
+	return nil
+}
+func (s *stubCodeIndex) SearchChunks(string, int) []*core.CodeChunk { return s.chunks }
+func (s *stubCodeIndex) BuildIndex(context.Context) error           { return nil }
+func (s *stubCodeIndex) UpdateIncremental([]string) error           { return nil }
+func (s *stubCodeIndex) Save() error                                { return nil }
+func (s *stubCodeIndex) Version() string                            { return "" }
 
 type stubSemanticStore struct{}
 
-func (*stubSemanticStore) Query(context.Context, string, int) ([]VectorMatch, error) { return nil, nil }
+func (*stubSemanticStore) Query(context.Context, string, int) ([]search.VectorMatch, error) {
+	return nil, nil
+}
 
 func TestSearchWithBudgetUsesSummaryFallback(t *testing.T) {
 	index := &stubCodeIndex{
-		chunks: []*CodeChunk{
+		chunks: []*core.CodeChunk{
 			{
 				ID:         "chunk-1",
 				File:       "service.go",
@@ -45,11 +55,11 @@ func TestSearchWithBudgetUsesSummaryFallback(t *testing.T) {
 			},
 		},
 	}
-	engine := NewSearchEngine(&stubSemanticStore{}, index)
+	engine := search.NewSearchEngine(&stubSemanticStore{}, index)
 
-	results, err := engine.SearchWithBudget(SearchQuery{
+	results, err := engine.SearchWithBudget(search.SearchQuery{
 		Text:       "service",
-		Mode:       SearchKeyword,
+		Mode:       search.SearchKeyword,
 		MaxResults: 1,
 	}, 50)
 	if err != nil {
