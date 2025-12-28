adfc4167163b4f58b4629d9270e7eb1e21fbf51a
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_awk.go

--- tools/cli_nix/text/cli_awk.go
+++ tools/cli_nix/text/cli_awk.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewAwkTool exposes the awk CLI.
-func NewAwkTool(basePath string) framework.Tool {
+func NewAwkTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_awk",
 		Description: "Runs awk for advanced text processing.",
