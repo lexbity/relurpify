cbb1089f2cc4dd2b1170ccb0fabaa950d09f8087
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_yq.go

--- tools/cli_nix/text/cli_yq.go
+++ tools/cli_nix/text/cli_yq.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewYQTool exposes the yq CLI.
-func NewYQTool(basePath string) framework.Tool {
+func NewYQTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_yq",
 		Description: "Processes YAML content using yq.",
