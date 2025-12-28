fab3c1e14e0358d2462d0c90467d356f911e97a9
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/archive/cli_gzip.go

--- tools/cli_nix/archive/cli_gzip.go
+++ tools/cli_nix/archive/cli_gzip.go
@@ -1,12 +1,12 @@
 package archive
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewGzipTool exposes the gzip CLI.
-func NewGzipTool(basePath string) framework.Tool {
+func NewGzipTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_gzip",
 		Description: "Compresses data with gzip.",
