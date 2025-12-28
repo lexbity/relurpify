c7894f0280d19289cf7e028726a95bded00eb977
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_echo.go

--- tools/cli_nix/text/cli_echo.go
+++ tools/cli_nix/text/cli_echo.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewEchoTool exposes the echo CLI utility.
-func NewEchoTool(basePath string) framework.Tool {
+func NewEchoTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_echo",
 		Description: "Writes text to standard output using echo.",
