49a5f8cd8f71d573359e73222a1daf5ac7607bcb
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/system/cli_strace.go

--- tools/cli_nix/system/cli_strace.go
+++ tools/cli_nix/system/cli_strace.go
@@ -1,12 +1,12 @@
 package system
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewStraceTool exposes the strace CLI.
-func NewStraceTool(basePath string) framework.Tool {
+func NewStraceTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_strace",
 		Description: "Traces syscalls made by a process using strace.",
