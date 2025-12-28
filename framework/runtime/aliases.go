6dac9f83044a52e7bdcb238d9766c4e72f0b90dc
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Add framework/runtime/aliases.go

---
+++ framework/runtime/aliases.go
@@ -0,0 +1,48 @@
+package runtime
+
+import (
+	"github.com/lexcodex/relurpify/framework/core"
+	"github.com/lexcodex/relurpify/framework/graph"
+	"github.com/lexcodex/relurpify/framework/manifest"
+)
+
+type PermissionType = core.PermissionType
+type FileSystemAction = core.FileSystemAction
+type FileSystemPermission = core.FileSystemPermission
+type ExecutablePermission = core.ExecutablePermission
+type NetworkPermission = core.NetworkPermission
+type CapabilityPermission = core.CapabilityPermission
+type IPCPermission = core.IPCPermission
+type PermissionSet = core.PermissionSet
+type PermissionDescriptor = core.PermissionDescriptor
+type PermissionDeniedError = core.PermissionDeniedError
+type ToolPermissions = core.ToolPermissions
+type AgentManifest = manifest.AgentManifest
+type Agent = graph.Agent
+type Context = core.Context
+type Result = core.Result
+type Task = core.Task
+type Config = core.Config
+type AuditLogger = core.AuditLogger
+type AuditQuery = core.AuditQuery
+type AuditRecord = core.AuditRecord
+type Tool = core.Tool
+
+const (
+	PermissionTypeFilesystem = core.PermissionTypeFilesystem
+	PermissionTypeExecutable = core.PermissionTypeExecutable
+	PermissionTypeNetwork    = core.PermissionTypeNetwork
+	PermissionTypeCapability = core.PermissionTypeCapability
+	PermissionTypeIPC        = core.PermissionTypeIPC
+	PermissionTypeHITL       = core.PermissionTypeHITL
+)
+
+const (
+	FileSystemRead    = core.FileSystemRead
+	FileSystemWrite   = core.FileSystemWrite
+	FileSystemExecute = core.FileSystemExecute
+	FileSystemList    = core.FileSystemList
+)
+
+var LoadAgentManifest = manifest.LoadAgentManifest
+var NewInMemoryAuditLogger = core.NewInMemoryAuditLogger
