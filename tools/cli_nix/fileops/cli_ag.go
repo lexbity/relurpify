69cbf0895f7a61d60605e3cfb01f7a895b40826d
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/fileops/cli_ag.go

--- tools/cli_nix/fileops/cli_ag.go
+++ tools/cli_nix/fileops/cli_ag.go
@@ -1,12 +1,12 @@
 package fileops
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewAGTool exposes the ag CLI.
-func NewAGTool(basePath string) framework.Tool {
+func NewAGTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_ag",
 		Description: "Searches codebases with the silver searcher (ag).",
