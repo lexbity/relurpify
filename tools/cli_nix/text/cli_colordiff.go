6ded92d4dff9edb582ddec30988dbb0e93cc3b86
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_colordiff.go

--- tools/cli_nix/text/cli_colordiff.go
+++ tools/cli_nix/text/cli_colordiff.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewColordiffTool exposes the colordiff CLI.
-func NewColordiffTool(basePath string) framework.Tool {
+func NewColordiffTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_colordiff",
 		Description: "Shows colorized diffs using colordiff.",
