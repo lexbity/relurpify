5e94baddcb3ef2c1020b6a0636248534337c24d7
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_sort.go

--- tools/cli_nix/text/cli_sort.go
+++ tools/cli_nix/text/cli_sort.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewSortTool exposes the sort CLI.
-func NewSortTool(basePath string) framework.Tool {
+func NewSortTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_sort",
 		Description: "Sorts lines of text with the sort utility.",
