a7401cf4d3f9d456e003a9769b28584c8bf5eb19
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/fileops/cli_stat.go

--- tools/cli_nix/fileops/cli_stat.go
+++ tools/cli_nix/fileops/cli_stat.go
@@ -1,12 +1,12 @@
 package fileops
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewStatTool exposes the stat CLI.
-func NewStatTool(basePath string) framework.Tool {
+func NewStatTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_stat",
 		Description: "Shows file metadata with stat.",
