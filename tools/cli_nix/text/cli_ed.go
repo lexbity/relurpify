24c074d56c366d7b75995e3820cb1dd574255168
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_ed.go

--- tools/cli_nix/text/cli_ed.go
+++ tools/cli_nix/text/cli_ed.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewEdTool exposes the ed CLI.
-func NewEdTool(basePath string) framework.Tool {
+func NewEdTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_ed",
 		Description: "Runs the ed line editor for scripted edits.",
