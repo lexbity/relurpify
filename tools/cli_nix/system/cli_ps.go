7d1110cda681e989478f472f2895800e4efc98b9
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/system/cli_ps.go

--- tools/cli_nix/system/cli_ps.go
+++ tools/cli_nix/system/cli_ps.go
@@ -1,12 +1,12 @@
 package system
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewPSTool exposes the ps CLI.
-func NewPSTool(basePath string) framework.Tool {
+func NewPSTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_ps",
 		Description: "Inspects running processes via ps.",
