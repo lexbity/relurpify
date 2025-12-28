3e41e6a3a851a7047a86eb38131cf2705598fa34
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_wc.go

--- tools/cli_nix/text/cli_wc.go
+++ tools/cli_nix/text/cli_wc.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewWCTool exposes the wc CLI.
-func NewWCTool(basePath string) framework.Tool {
+func NewWCTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_wc",
 		Description: "Counts lines, words, and bytes via wc.",
