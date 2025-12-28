5e5a11df8cf36e9683160f25084d380ab6518fca
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_ex.go

--- tools/cli_nix/text/cli_ex.go
+++ tools/cli_nix/text/cli_ex.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewExTool exposes the ex CLI.
-func NewExTool(basePath string) framework.Tool {
+func NewExTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_ex",
 		Description: "Executes ex for vi-style batch editing.",
