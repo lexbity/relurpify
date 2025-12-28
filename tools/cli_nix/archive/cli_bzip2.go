e65683685ffc6b7a3aed420832909513b5231d9b
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/archive/cli_bzip2.go

--- tools/cli_nix/archive/cli_bzip2.go
+++ tools/cli_nix/archive/cli_bzip2.go
@@ -1,12 +1,12 @@
 package archive
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewBzip2Tool exposes the bzip2 CLI.
-func NewBzip2Tool(basePath string) framework.Tool {
+func NewBzip2Tool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_bzip2",
 		Description: "Compresses data using bzip2.",
