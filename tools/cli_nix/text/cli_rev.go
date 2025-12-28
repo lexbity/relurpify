aedaf6d48e1e43c5caab9191892acf64f5270bc4
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_rev.go

--- tools/cli_nix/text/cli_rev.go
+++ tools/cli_nix/text/cli_rev.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewRevTool exposes the rev CLI.
-func NewRevTool(basePath string) framework.Tool {
+func NewRevTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_rev",
 		Description: "Reverses lines character-wise using rev.",
