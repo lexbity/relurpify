899e7658b4b38855248ef31dd25533758705055d
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/fileops/cli_fd.go

--- tools/cli_nix/fileops/cli_fd.go
+++ tools/cli_nix/fileops/cli_fd.go
@@ -1,12 +1,12 @@
 package fileops
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewFDTool exposes the fd CLI.
-func NewFDTool(basePath string) framework.Tool {
+func NewFDTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_fd",
 		Description: "Performs fast file searches with fd.",
