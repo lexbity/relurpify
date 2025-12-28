528cfb4e7d32e594e022f46a8934164a93ae7fa1
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/network/cli_nc.go

--- tools/cli_nix/network/cli_nc.go
+++ tools/cli_nix/network/cli_nc.go
@@ -1,12 +1,12 @@
 package network
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewNCTool exposes the nc CLI.
-func NewNCTool(basePath string) framework.Tool {
+func NewNCTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_nc",
 		Description: "Creates TCP/UDP connections via netcat (nc).",
