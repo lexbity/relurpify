488a2e903f55c598b3748b7cb0b375f9920d3f56
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/archive/cli_tar.go

--- tools/cli_nix/archive/cli_tar.go
+++ tools/cli_nix/archive/cli_tar.go
@@ -1,12 +1,12 @@
 package archive
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewTarTool exposes the tar CLI.
-func NewTarTool(basePath string) framework.Tool {
+func NewTarTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_tar",
 		Description: "Creates or extracts tar archives.",
