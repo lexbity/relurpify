d4756f18c951e509ed36dd4b9c5b1d3a7df44bc3
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/system/cli_lsof.go

--- tools/cli_nix/system/cli_lsof.go
+++ tools/cli_nix/system/cli_lsof.go
@@ -1,12 +1,12 @@
 package system
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewLsofTool exposes the lsof CLI.
-func NewLsofTool(basePath string) framework.Tool {
+func NewLsofTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_lsof",
 		Description: "Lists open files and sockets via lsof.",
