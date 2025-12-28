ef80f0c14862b1d54d811fb73b97738468c9409b
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_column.go

--- tools/cli_nix/text/cli_column.go
+++ tools/cli_nix/text/cli_column.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewColumnTool exposes the column CLI.
-func NewColumnTool(basePath string) framework.Tool {
+func NewColumnTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_column",
 		Description: "Formats text into aligned columns.",
