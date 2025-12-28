5b1a7ae8f6ae5df83eaa1adfd15aa64f8f3c2c02
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/system/cli_du.go

--- tools/cli_nix/system/cli_du.go
+++ tools/cli_nix/system/cli_du.go
@@ -1,12 +1,12 @@
 package system
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewDUTool exposes the du CLI.
-func NewDUTool(basePath string) framework.Tool {
+func NewDUTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_du",
 		Description: "Summarizes directory usage with du.",
