41443a5633d8d61adf9a26d6deff84bbdd9b66a3
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/system/cli_htop.go

--- tools/cli_nix/system/cli_htop.go
+++ tools/cli_nix/system/cli_htop.go
@@ -1,12 +1,12 @@
 package system
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewHtopTool exposes the htop CLI.
-func NewHtopTool(basePath string) framework.Tool {
+func NewHtopTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_htop",
 		Description: "Runs htop for interactive process monitoring.",
