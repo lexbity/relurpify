05fb3c16e177adf228a515d9ecf86e4ea92179e1
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_uniq.go

--- tools/cli_nix/text/cli_uniq.go
+++ tools/cli_nix/text/cli_uniq.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewUniqTool exposes the uniq CLI.
-func NewUniqTool(basePath string) framework.Tool {
+func NewUniqTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_uniq",
 		Description: "Filters or counts duplicate lines with uniq.",
