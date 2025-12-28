4468598d20ef0615edaa5386edbfbac7cbdc6fab
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_xxd.go

--- tools/cli_nix/text/cli_xxd.go
+++ tools/cli_nix/text/cli_xxd.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewXxdTool exposes the xxd CLI.
-func NewXxdTool(basePath string) framework.Tool {
+func NewXxdTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_xxd",
 		Description: "Creates hex dumps with xxd.",
