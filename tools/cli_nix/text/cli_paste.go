a9267cd460c0a568f1910e61b20ec4f2812549ce
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_paste.go

--- tools/cli_nix/text/cli_paste.go
+++ tools/cli_nix/text/cli_paste.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewPasteTool exposes the paste CLI.
-func NewPasteTool(basePath string) framework.Tool {
+func NewPasteTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_paste",
 		Description: "Combines lines from files using paste.",
