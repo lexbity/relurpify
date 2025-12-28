415d1afcbbfdc73f125a27007e213f695d8c74a4
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_patch.go

--- tools/cli_nix/text/cli_patch.go
+++ tools/cli_nix/text/cli_patch.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewPatchTool exposes the patch CLI.
-func NewPatchTool(basePath string) framework.Tool {
+func NewPatchTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_patch",
 		Description: "Applies unified diffs using patch.",
