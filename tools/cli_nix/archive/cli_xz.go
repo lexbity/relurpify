3e9b0f37e02ee5d655f8f0ef7804cf007a42b0aa
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/archive/cli_xz.go

--- tools/cli_nix/archive/cli_xz.go
+++ tools/cli_nix/archive/cli_xz.go
@@ -1,12 +1,12 @@
 package archive
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewXzTool exposes the xz CLI.
-func NewXzTool(basePath string) framework.Tool {
+func NewXzTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_xz",
 		Description: "Compresses data with xz.",
