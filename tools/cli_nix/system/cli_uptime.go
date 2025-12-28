89dc72a1691847c29335b3169520d4c65b652b06
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/system/cli_uptime.go

--- tools/cli_nix/system/cli_uptime.go
+++ tools/cli_nix/system/cli_uptime.go
@@ -1,12 +1,12 @@
 package system
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewUptimeTool exposes the uptime CLI.
-func NewUptimeTool(basePath string) framework.Tool {
+func NewUptimeTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_uptime",
 		Description: "Shows system uptime information.",
