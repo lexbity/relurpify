2db10ed2af4da0b55ace74ec07becd34d7d198a9
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_comm.go

--- tools/cli_nix/text/cli_comm.go
+++ tools/cli_nix/text/cli_comm.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewCommTool exposes the comm CLI.
-func NewCommTool(basePath string) framework.Tool {
+func NewCommTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_comm",
 		Description: "Compares sorted files using comm.",
