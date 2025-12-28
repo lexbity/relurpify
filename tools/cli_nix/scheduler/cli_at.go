c09eca90f5eff8c647eeaa6e81bff9a621798d34
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/scheduler/cli_at.go

--- tools/cli_nix/scheduler/cli_at.go
+++ tools/cli_nix/scheduler/cli_at.go
@@ -1,12 +1,12 @@
 package scheduler
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewAtTool exposes the at CLI.
-func NewAtTool(basePath string) framework.Tool {
+func NewAtTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_at",
 		Description: "Schedules one-off jobs using at.",
