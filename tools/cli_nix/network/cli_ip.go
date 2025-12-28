07e6ab5f9facacfe621badaee110af6ca4885c92
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/network/cli_ip.go

--- tools/cli_nix/network/cli_ip.go
+++ tools/cli_nix/network/cli_ip.go
@@ -1,12 +1,12 @@
 package network
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewIPTool exposes the ip CLI.
-func NewIPTool(basePath string) framework.Tool {
+func NewIPTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_ip",
 		Description: "Manages network interfaces with ip.",
