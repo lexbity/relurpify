4bad506f0af083206ec64faf490557af481945dc
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/build/cli_cmake.go

--- tools/cli_nix/build/cli_cmake.go
+++ tools/cli_nix/build/cli_cmake.go
@@ -1,12 +1,12 @@
 package build
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewCMakeTool exposes the cmake CLI.
-func NewCMakeTool(basePath string) framework.Tool {
+func NewCMakeTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_cmake",
 		Description: "Configures builds with cmake.",
