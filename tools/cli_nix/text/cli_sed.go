e7cbf98110c4ffce58a2923a64ca539de42f42dd
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_sed.go

--- tools/cli_nix/text/cli_sed.go
+++ tools/cli_nix/text/cli_sed.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewSedTool exposes the sed CLI.
-func NewSedTool(basePath string) framework.Tool {
+func NewSedTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_sed",
 		Description: "Runs sed for stream editing tasks.",
