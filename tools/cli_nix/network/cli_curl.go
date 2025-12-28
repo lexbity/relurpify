f5300ef63cebb00d985f7c892cdb53f8d7b2c9fb
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/network/cli_curl.go

--- tools/cli_nix/network/cli_curl.go
+++ tools/cli_nix/network/cli_curl.go
@@ -1,12 +1,12 @@
 package network
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewCurlTool exposes the curl CLI.
-func NewCurlTool(basePath string) framework.Tool {
+func NewCurlTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_curl",
 		Description: "Transfers data over HTTP(S) using curl.",
