22418a42f3af04af9eed5c79038d0c60fa904354
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/build/cli_sqlite3.go

--- tools/cli_nix/build/cli_sqlite3.go
+++ tools/cli_nix/build/cli_sqlite3.go
@@ -1,12 +1,12 @@
 package build
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewSQLite3Tool exposes sqlite3 for running SQL scripts inside the workspace.
-func NewSQLite3Tool(basePath string) framework.Tool {
+func NewSQLite3Tool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_sqlite3",
 		Description: "Executes SQLite commands using sqlite3 inside the workspace.",
@@ -14,4 +14,3 @@ func NewSQLite3Tool(basePath string) framework.Tool {
 		Category:    "cli_build",
 	})
 }
-
