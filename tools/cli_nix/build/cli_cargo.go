ef8e9dca9fda20cba3179fc96a5aad080d7e290f
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/build/cli_cargo.go

--- tools/cli_nix/build/cli_cargo.go
+++ tools/cli_nix/build/cli_cargo.go
@@ -1,12 +1,12 @@
 package build
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewCargoTool exposes the cargo CLI for Rust builds.
-func NewCargoTool(basePath string) framework.Tool {
+func NewCargoTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_cargo",
 		Description: "Executes Rust cargo commands inside the workspace.",
