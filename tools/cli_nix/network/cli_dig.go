ec45b58b55848a92df8a1a3c4655389580c4dac2
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/network/cli_dig.go

--- tools/cli_nix/network/cli_dig.go
+++ tools/cli_nix/network/cli_dig.go
@@ -1,12 +1,12 @@
 package network
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewDigTool exposes the dig CLI.
-func NewDigTool(basePath string) framework.Tool {
+func NewDigTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_dig",
 		Description: "Queries DNS records using dig.",
