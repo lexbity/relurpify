cf46870bdd89f9fd351381d7a124e7751816beca
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_jq.go

--- tools/cli_nix/text/cli_jq.go
+++ tools/cli_nix/text/cli_jq.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewJQTool exposes the jq CLI.
-func NewJQTool(basePath string) framework.Tool {
+func NewJQTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_jq",
 		Description: "Queries or transforms JSON using jq.",
