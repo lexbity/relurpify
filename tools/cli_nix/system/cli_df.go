1c4199727de6d6960c21934163695f3b6c29aa8a
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/system/cli_df.go

--- tools/cli_nix/system/cli_df.go
+++ tools/cli_nix/system/cli_df.go
@@ -1,12 +1,12 @@
 package system
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewDFTool exposes the df CLI.
-func NewDFTool(basePath string) framework.Tool {
+func NewDFTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_df",
 		Description: "Reports disk usage statistics with df.",
