1d8c97f8f8f004cbd172d4cd2e4a540e1ac513cb
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/text/cli_diff.go

--- tools/cli_nix/text/cli_diff.go
+++ tools/cli_nix/text/cli_diff.go
@@ -1,12 +1,12 @@
 package text
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewDiffTool exposes the diff CLI.
-func NewDiffTool(basePath string) framework.Tool {
+func NewDiffTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_diff",
 		Description: "Runs diff to compare files.",
