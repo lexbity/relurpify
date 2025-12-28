2809b3d97659fc116b3fec626ee83679603dc221
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/fileops/cli_file.go

--- tools/cli_nix/fileops/cli_file.go
+++ tools/cli_nix/fileops/cli_file.go
@@ -1,12 +1,12 @@
 package fileops
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewFileTool exposes the file CLI.
-func NewFileTool(basePath string) framework.Tool {
+func NewFileTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_file",
 		Description: "Detects file types using the file command.",
