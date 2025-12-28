1a3b72dbfe53abd19fd26d3a74854dec716e973d
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/network/cli_ss.go

--- tools/cli_nix/network/cli_ss.go
+++ tools/cli_nix/network/cli_ss.go
@@ -1,12 +1,12 @@
 package network
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewSSTool exposes the ss CLI.
-func NewSSTool(basePath string) framework.Tool {
+func NewSSTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_ss",
 		Description: "Inspects sockets using ss.",
