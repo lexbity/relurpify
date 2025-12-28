d78b8f5ae6054ee75b7d6734063a196d7f0dee3b
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:03 -0800

    Update tools/cli_nix/system/cli_time.go

--- tools/cli_nix/system/cli_time.go
+++ tools/cli_nix/system/cli_time.go
@@ -1,12 +1,12 @@
 package system
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewTimeTool exposes the time CLI.
-func NewTimeTool(basePath string) framework.Tool {
+func NewTimeTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_time",
 		Description: "Times the execution of commands with /usr/bin/time.",
