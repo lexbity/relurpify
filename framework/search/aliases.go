e387ec814db01e02b2a25902223f7e491550e0bf
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:04 -0800

    Add framework/search/aliases.go

---
+++ framework/search/aliases.go
@@ -0,0 +1,11 @@
+package search
+
+import "github.com/lexcodex/relurpify/framework/core"
+
+var estimateTokens = core.EstimateTokens
+
+type CodeIndex = core.CodeIndex
+type CodeChunk = core.CodeChunk
+type SymbolLocation = core.SymbolLocation
+
+const permissionMatchAll = "**"
