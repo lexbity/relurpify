7c425ea3e867dde267b0de2d7e21f0a08cde762a
prestin liu <liuprestin@gmail.com>
2025-12-28 15:45:02 -0800

    Update tools/cli_nix/build/cli_python.go

--- tools/cli_nix/build/cli_python.go
+++ tools/cli_nix/build/cli_python.go
@@ -1,12 +1,12 @@
 package build
 
 import (
-	"github.com/lexcodex/relurpify/framework"
+	"github.com/lexcodex/relurpify/framework/core"
 	clinix "github.com/lexcodex/relurpify/tools/cli_nix"
 )
 
 // NewPythonTool exposes python3 for running Python inside the workspace.
-func NewPythonTool(basePath string) framework.Tool {
+func NewPythonTool(basePath string) core.Tool {
 	return clinix.NewCommandTool(basePath, clinix.CommandToolConfig{
 		Name:        "cli_python",
 		Description: "Executes Python commands using python3 inside the workspace.",
@@ -14,4 +14,3 @@ func NewPythonTool(basePath string) framework.Tool {
 		Category:    "cli_build",
 	})
 }
-
