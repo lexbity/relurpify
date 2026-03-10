5a610687ec43b71d6a358cfe75d3d3ee67a8ec0f
prestin liu <liuprestin@gmail.com>
2026-03-10 14:35:34 -0700

    Deleted documentation and reworked framework files

--- framework/core/contexttypes/context_payload.go
+++
@@ -1,8 +0,0 @@
-package core
-
-// ContextFileContent captures explicit file context supplied by callers.
-type ContextFileContent struct {
-	Path      string `json:"path"`
-	Content   string `json:"content"`
-	Truncated bool   `json:"truncated"`
-}
